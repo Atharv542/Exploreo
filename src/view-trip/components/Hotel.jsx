d1f6104519afeb9056d22fc7b887abdfb887fc23
Atharv542 <162612781+Atharv542@users.noreply.github.com>
2025-04-02 21:07:58 +0530

    Update Hotel.jsx

--- src/view-trip/components/Hotel.jsx
+++ src/view-trip/components/Hotel.jsx
@@ -4,14 +4,15 @@ import HotelCardItem from "./HotelCardItem";
 
 function Hotel({ trip }) {
   return (
-    <div>
-      <h2 className="font-bold text-xl mt-5">Hotel Recommendation</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
-        {trip?.tripData?.travelPlan?.hotelOptions?.map((hotel, index) => (
-          <HotelCardItem hotel={hotel} />
-        ))}
-      </div>
-    </div>
+<div>
+  <h2 className="font-bold text-xl mt-5">Hotel Recommendation</h2>
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+    {trip?.tripData?.travelPlan?.hotelOptions?.map((hotel, index) => (
+      <HotelCardItem key={index} hotel={hotel} />
+    ))}
+  </div>
+</div>
+
   );
 }
 
